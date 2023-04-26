1c4f55672cd40a7b7c9764b5df3be5bc6d48aa68
IrinaT <irina.tiora@isa.utm.md>
2023-04-26 20:22:23 +0300

    add little reaction game in vue

---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer"> click !!! {{ this.reactionTime == 0 ? "" : this.reactionTime  }}</div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    updated() {
+        console.log("showBlock updated")
+    },
+    unmounted() {
+        console.log("Unmounted")
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => { this.reactionTime += 10 }, 10)
+        },
+        stopTimer() {
+           clearInterval(this.timer) 
+           console.log(this.reactionTime)
+           this.$emit('end',this.reactionTime)
+        }
+    }
+}
+</script>
+
+
+<style>
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    ;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,27 @@
+<template>
+    <p>Reaction time - {{ score }} ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+  
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = "Ninja skills"
+        } else if (this.score < 500) {
+            this.rank = "Wave skills"
+        } else if (this.score < 600) {
+            this.rank = "Turtle skills"
+        }
+    }
+}
+
+</script>
+
+<style></style>
