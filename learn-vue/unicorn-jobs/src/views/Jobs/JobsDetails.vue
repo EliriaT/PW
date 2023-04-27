c61145bc200e71a98dd9189b532685a880f2f427
IrinaT <irina.tiora@isa.utm.md>
2023-04-27 10:53:47 +0300

    Fetch jobs from local api

--- learn-vue/unicorn-jobs/src/views/Jobs/JobsDetails.vue
+++ learn-vue/unicorn-jobs/src/views/Jobs/JobsDetails.vue
@@ -1,19 +1,29 @@
 <template>
-   <h1>Job</h1>
-   <p>The job id is {{ id }}</p>
-  </template>
+    <div v-if="job">
+        <h1>{{ job.title }}</h1>
+        <p>The job id is {{ id }}</p>
+        <p>{{ job.details }}</p>
+    </div>
+    <div v-else> Loading job details...</div>
+</template>
   
-  <script>
-  export default {
-    props:['id'],
+<script>
+export default {
+    props: ['id'],
     data() {
-    return {
-    //   id: this.$route.params.id
+        return {
+            //   id: this.$route.params.id
+            job: null //sau job:''
+        }
+
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+            .then(res => res.json())
+            .then(data => this.job = data)
+            .catch(err => console.log(err.message))
     }
-  }
-  }
-  </script>
+}
+</script>
   
-  <style>
-  
-  </style>
+<style></style>
